56e88820c0ea91d3536ebf1144577bb1868d1736
souvik <souvik@fusioncharts.com>
2015-07-23 18:56:08 +0530

    mortgage

--- lesson/mortgage/mortgage.js
+++ lesson/mortgage/mortgage.js
@@ -1,3 +1,22 @@
+function on(ev)
+{
+    if(ev.value==ev.defaultValue)
+    	{ 
+    		ev.value=""; 
+    		ev.style.color="#000";
+    	 }
+}
+function off(ev)
+{
+    if(ev.value=="")
+    	{ 
+    		ev.value=ev.defaultValue;
+    		 ev.style.color="#888";
+        }
+}
+
+
+
 function cla_loan()
 {
    var l=document.getElementById("loan").value;
@@ -8,21 +27,21 @@ function cla_loan()
    var R=eval(r)/100;
    	   R=R/12;
    var R1=Math.pow((1+R),eval(t));
-   if(e==="")
+   if(e==="RS")
    {
    	 
    	   e=l*R*R1;
    	   e=e/(R1-1);
    	   document.getElementById("emi").value=e;   
    }
-   if(l==="")
+   if(l==="RS")
    {
       l=(e*(R1-1))/(R*R1);
       document.getElementById("loan").value=l;   
 
    }
    
-   if(t==="")
+   if(t==="month")
    {
    	t=Math.log(e/(e-l*R))/Math.log(1+R);
    	document.getElementById("time").value=t;   
