f63df8b41960135f4bf8e30d0b4bf69bbd35cf78
souvik <souvik@fusioncharts.com>
2015-07-24 18:47:56 +0530

    mort

--- lesson/mortgage/mortgage.js
+++ lesson/mortgage/mortgage.js
@@ -19,31 +19,33 @@ function off(ev)
 
 function cla_loan()
 {
+   var R,R1;
    var l=document.getElementById("loan").value;
    var r=document.getElementById("rate").value;
    var t=document.getElementById("time").value;
    var e=document.getElementById("emi").value;
    console.log(e);
-   var R=eval(r)/100;
+  
+       R=eval(r)/100;
    	   R=R/12;
-   var R1=Math.pow((1+R),eval(t));
+   
    if(e==="RS")
    {
-   	 
+   	   R1=Math.pow((1+R),eval(t));
    	   e=l*R*R1;
    	   e=e/(R1-1);
    	   document.getElementById("emi").value=e;   
    }
    if(l==="RS")
    {
+      R1=Math.pow((1+R),eval(t));
       l=(e*(R1-1))/(R*R1);
       document.getElementById("loan").value=l;   
-
    }
    
-   if(t==="month")
+if(t==="month")
    {
-   	t=Math.log(e/(e-l*R))/Math.log(1+R);
+    	t=Math.log(e/(e-l*R))/Math.log(1+R);
    	document.getElementById("time").value=t;   
    }
 }
